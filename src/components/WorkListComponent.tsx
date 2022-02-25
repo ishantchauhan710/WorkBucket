0cd73367caa9689f1e5d0b4381450a5ba6acfc66
ishant <ishantchauhan710@gmail.com>
2022-02-25 21:29:09 +0530

    Bucket1 Name Changing Feature Implemented

--- src/components/WorkListComponent.tsx
+++ src/components/WorkListComponent.tsx
@@ -18,6 +18,14 @@ const WorkListComponent: React.FC<Props> = ({workList,setWorkList,completedWorkL
   const [editBucket1NameMode, setEditBucket1NameMode] = useState<boolean>(false);
   const [editBucket2NameMode, setEditBucket2NameMode] = useState<boolean>(false);
 
+  const toggleBucket1EditMode = () => {
+    if(editBucket1NameMode) {
+      setEditBucket1NameMode(false)
+    } else {
+      setEditBucket1NameMode(true)
+    }
+  }
+
   const toggleBucket2EditMode = () => {
     if(editBucket2NameMode) {
       setEditBucket2NameMode(false)
@@ -25,6 +33,8 @@ const WorkListComponent: React.FC<Props> = ({workList,setWorkList,completedWorkL
       setEditBucket2NameMode(true)
     }
   }
+
+  
   
   
 
@@ -35,8 +45,8 @@ const WorkListComponent: React.FC<Props> = ({workList,setWorkList,completedWorkL
       { (provided,snapshot) => (
            <div ref={provided.innerRef} className={`work-list-container1 ${snapshot.isDraggingOver?'dragActive':''}`} {...provided.droppableProps}>
             <div className='bucket1-container'>
-              <span className='bucket1-title'>{bucket1Name}</span>
-              <button className='btn-edit-bucket1-title'><i className='material-icons'>edit</i></button>
+            <span className='bucket1-title'>{editBucket1NameMode?(<input type="input" value={bucket1Name} onChange={(e)=>setBucket1Name(e.target.value)} className='input-edit-work-bucket-title' />):(bucket1Name)}</span>
+              <button onClick={() => toggleBucket1EditMode()} className='btn-edit-bucket1-title'><i className='material-icons'>{editBucket1NameMode?('check'):('edit')}</i></button>
             </div>
               {
                 workList?.map((work: Work, index) => (
@@ -54,8 +64,8 @@ const WorkListComponent: React.FC<Props> = ({workList,setWorkList,completedWorkL
      <Droppable droppableId='bucket2'>
       { (provided,snapshot) => (
            <div ref={provided.innerRef}  className={`work-list-container2 ${snapshot.isDraggingOver?'dragActive':''}`} {...provided.droppableProps}>
-           <div className='bucket1-container'>
-             <span className='bucket1-title'>{editBucket2NameMode?(<input type="input" value={bucket2Name} onChange={(e)=>setBucket2Name(e.target.value)} className='input-edit-work-bucket-title' />):(bucket2Name)}</span>
+           <div className='bucket2-container'>
+             <span className='bucket2-title'>{editBucket2NameMode?(<input type="input" value={bucket2Name} onChange={(e)=>setBucket2Name(e.target.value)} className='input-edit-work-bucket-title' />):(bucket2Name)}</span>
              <button onClick={() => toggleBucket2EditMode()} className='btn-edit-bucket2-title'><i className='material-icons'>{editBucket2NameMode?('check'):('edit')}</i></button>
            </div>
              {
